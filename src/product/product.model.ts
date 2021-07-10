ddd9ad09af6e7c54e47c455c10cf6633b5f12acc
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-07-10 15:52:03 +0300

    db model product

--- src/product/product.model.ts
+++ src/product/product.model.ts
@@ -1,17 +1,50 @@
-export class ProductModel {
-  _id: string;
+import { prop } from '@typegoose/typegoose';
+import { TimeStamps, Base } from '@typegoose/typegoose/lib/defaultClasses';
+
+class ProductSpecification {
+  @prop()
+  name: string;
+
+  @prop()
+  value: string;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface ProductModel extends Base {}
+export class ProductModel extends TimeStamps {
+  @prop()
   image: string;
+
+  @prop()
   title: string;
+
+  @prop()
   price: number;
+
+  @prop()
   oldNumber: number;
+
+  @prop()
   credit: number;
+
+  @prop()
   calculatedRating: number;
+
+  @prop()
   description: string;
+
+  @prop()
   advantages: string;
+
+  @prop()
   disAdvantages: string;
+
+  @prop({ type: () => [String] })
   categories: string[];
+
+  @prop({ type: () => [String] })
   tags: string[];
-  specifications: {
-    [key: string]: string;
-  };
+
+  @prop({ type: () => [ProductSpecification], _id: false })
+  specifications: ProductSpecification[];
 }
