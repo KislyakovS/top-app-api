cb7ff6d4c36857b4dc7470f54cf1a3277845a2e3
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-07-10 16:35:36 +0300

    Update review model

--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,8 +1,9 @@
 import { prop } from '@typegoose/typegoose';
 import { TimeStamps, Base } from '@typegoose/typegoose/lib/defaultClasses';
+import { Types } from 'mongoose';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface ReviewModel extends Base {}
+export interface ReviewModel extends Base { }
 export class ReviewModel extends TimeStamps {
   @prop()
   name: string;
@@ -15,4 +16,7 @@ export class ReviewModel extends TimeStamps {
 
   @prop()
   rating: number;
+
+  @prop()
+  productId: Types.ObjectId;
 }
