fba52ce199e97fe1dc9c91207ded56442a289e44
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-07-11 15:06:38 +0300

    Fix valid type product model

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -15,15 +15,19 @@ import { FindProductDto } from './dto/find-product.dto';
 
 @Controller('product')
 export class ProductController {
+  @UsePipes(new ValidationPipe())
   @Post('create')
   async create(@Body() dto: Omit<ProductModel, '_id'>) { }
 
+  @UsePipes(new ValidationPipe())
   @Get(':id')
   async get(@Param('id') id: string) { }
 
+  @UsePipes(new ValidationPipe())
   @Delete(':id')
   async delete(@Param('id') id: string) { }
 
+  @UsePipes(new ValidationPipe())
   @Patch(':id')
   async update(@Param('id') id: string, @Body() dto: ProductModel) { }
 
