ef8a99df5750789c6ccbc0e1ae67a9ff55dca74d
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-07-11 15:10:51 +0300

    Validation top page dto

--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -7,25 +7,32 @@ import {
   Param,
   Patch,
   Post,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { TopPageModel } from './top-page.model';
 import { FindTopPageDto } from './dto/find-top-page.dto';
 
 @Controller('top-page')
 export class TopPageController {
+  @UsePipes(new ValidationPipe())
   @Post('create')
-  async create(@Body() dto: Omit<TopPageModel, '_id'>) {}
+  async create(@Body() dto: Omit<TopPageModel, '_id'>) { }
 
+  @UsePipes(new ValidationPipe())
   @Get(':id')
-  async get(@Param('id') id: string) {}
+  async get(@Param('id') id: string) { }
 
+  @UsePipes(new ValidationPipe())
   @Delete(':id')
-  async delete(@Param('id') id: string) {}
+  async delete(@Param('id') id: string) { }
 
+  @UsePipes(new ValidationPipe())
   @Patch(':id')
-  async update(@Param('id') id: string, @Body() dto: TopPageModel) {}
+  async update(@Param('id') id: string, @Body() dto: TopPageModel) { }
 
+  @UsePipes(new ValidationPipe())
   @HttpCode(200)
   @Post()
-  async find(@Body() dto: FindTopPageDto) {}
+  async find(@Body() dto: FindTopPageDto) { }
 }
--- src/top-page/top-page.model.ts
+++ src/top-page/top-page.model.ts
@@ -1,5 +1,7 @@
 import { prop } from '@typegoose/typegoose';
 import { TimeStamps, Base } from '@typegoose/typegoose/lib/defaultClasses';
+import { Type } from 'class-transformer';
+import { IsArray, IsEnum, IsString, ValidateNested } from 'class-validator';
 
 export enum TopLevelCategory {
   Courses,
@@ -31,35 +33,50 @@ export class TopPageAdvantage {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface TopPageModel extends Base {}
+export interface TopPageModel extends Base { }
 export class TopPageModel extends TimeStamps {
+  @IsEnum(TopLevelCategory)
   @prop({ enum: TopLevelCategory })
   firstCategory: TopLevelCategory;
 
+  @IsString()
   @prop()
   secondCategory: string;
 
+  @IsString()
   @prop({ unique: true })
   alias: string;
 
+  @IsString()
   @prop()
   title: string;
 
+  @IsString()
   @prop()
   category: string;
 
+  @ValidateNested()
+  @Type(() => HhData)
   @prop({ type: () => HhData })
   hh?: HhData;
 
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => TopPageAdvantage)
   @prop({ type: () => [TopPageAdvantage] })
   advantages: TopPageAdvantage[];
 
+  @IsString()
   @prop()
   seoText: string;
 
+  @IsString()
   @prop()
   tagsTitle: string;
 
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => String)
   @prop({ type: () => [String] })
   tags: string[];
 }
